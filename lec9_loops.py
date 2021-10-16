8e0dd49c818dc4d4bb4df905998c82321935676e
sarah mahmood <89476210+sarahmahmood99@users.noreply.github.com>
2021-10-16 05:15:29 +0300

    Create lec9_loops.py

---
+++ lec9_loops.py
@@ -0,0 +1,20 @@
+#for <var > in <sequences >
+for i in [0,1,2,3]:
+  print(i)
+
+print()
+
+for i in [1,3,5,7,9]:
+  print(i*i)
+
+print()
+
+for i in range(10): #from 0 to 9
+  print(i)
+
+print()
+
+for i in range(100): #from 0 to 99 
+  print(i)
+
+print()
