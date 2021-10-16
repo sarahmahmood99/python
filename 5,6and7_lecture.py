f7d0f83e93641ba973fee7f9c19bc291239426cd
sarah mahmood <89476210+sarahmahmood99@users.noreply.github.com>
2021-10-16 05:11:43 +0300

    Create 5,6and7_lecture.py

---
+++ 5,6and7_lecture.py
@@ -0,0 +1,44 @@
+names = "sarah mahmood"
+_names = "sarah"
+Names = "mahmood"
+
+print(names)
+print()
+print(_names)
+print()
+print(Names)
+print()
+
+x=5 
+print(x)
+print()
+
+x=4
+y=2+6 * x - (1+x)
+print(y)
+print()
+
+#this is comment one line    
+''' 
+this is comment many lines 
+and more 
+and more  
+'''
+
+print("sarah" +" " + "mahmood ")
+print()
+
+#print(<exp1> , <exp2>  , ......)
+print("sarah")  #statment 
+print()
+ 
+print(3+2)
+print(3,5,3+5)
+
+print("the answer is ", 10*10 )
+print()
+
+print("the answer is ", end="")
+print(3+2)
+
+
