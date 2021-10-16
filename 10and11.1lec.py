0d179fd3a6f685a5c0a32e25d61731b7a85ab23b
sarah mahmood <89476210+sarahmahmood99@users.noreply.github.com>
2021-10-16 05:16:28 +0300

    Create 10and11.1lec.py

---
+++ 10and11.1lec.py
@@ -0,0 +1,40 @@
+#lec10
+#to know the type of data
+print (type(3))
+print (type("sarah"))
+print (type(3.1))
+
+print(22/7)      #float
+print(22//7)     #int
+print(2*3)       #mul
+print(2**3)      #2^
+print(abs(-3))    #make it +
+print(22%7)     #22 mod 7 = 1 
+#/////////////////////////////////////////////
+#lec11
+print()
+
+x=5.0*2
+print(x)
+print(type(x))
+
+t=int(3.9)
+print(t)
+print(type(t))
+
+t=int(round(3.9))
+print(t)
+print(type(t))
+
+t=int(round(3.4))
+print(t)
+print(type(t))
+
+print()
+
+s=22/7
+a=round(s,2)
+print(a)
+
+a=round(s,6)
+print(a)
