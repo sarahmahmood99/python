51dd147a61f30bb37355e6eaf19665cf49825a8a
sarah mahmood <89476210+sarahmahmood99@users.noreply.github.com>
2021-10-16 05:10:37 +0300

    Create lec234_SDLC practice.py

---
+++ lec234_SDLC practice.py
@@ -0,0 +1,6 @@
+def main() :
+  celsius = eval(input("please enter celsius temperture?"))
+  fahernheit = (9/5) * celsius +32
+  print("the temperture is" , fahernheit ,"degree fahernheit")
+
+main()
